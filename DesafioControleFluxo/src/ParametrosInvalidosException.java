589443b0fb799da4ec2efa9736d23af7f62da92a
Cauan Augusto <cauan.au.i@gmail.com>
2024-06-02 19:58:05 -0300

    Modifield ParametrosInvalidosException

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,4 +1,15 @@
+/**
+ * Uma exceção personalizada que é lançada quando parâmetros inválidos são
+ * encontrados.
+ * Esta exceção é uma subclasse de {@link Exception}.
+ */
 public class ParametrosInvalidosException extends Exception {
+
+    /**
+     * Constrói uma nova {@code ParametrosInvalidosException} com a mensagem detalhada especificada.
+     *
+     * @param message - a mensagem detalhada, salva para posterior recuperação pelo método {@link #getMessage()}.
+     */
     public ParametrosInvalidosException(String message) {
         super(message);
     }
